b9dd3b221f6f9bab5a7074488df6f9439e7a579e
aidan mcbride <aidanmcbride@outlook.com>
2019-08-08 13:38:18 -0600

    Add essential pytest config
    Create conftest.py file containing a single global test fixture. Test
    fixture will create an instance of the flask application upon which unit
    tests can be run. As per TDD, the create_app function is yet to be
    written, and as such this test will fail if run.

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+"""
+Pytest configuration file:
+Imported before any tests are run
+contains global fixtures
+"""
+
+import pytest
+from flask import Flask
+
+@pytest.fixture
+def app():
+    """
+    create instance of application
+    """
+    from api import create_app
+    return create_app()
