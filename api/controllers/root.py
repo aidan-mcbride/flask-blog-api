f553e412745d65133d2696819433381689ba3a1c
aidan mcbride <aidanmcbride@outlook.com>
2019-08-09 09:20:39 -0600

    Remove root controller
    Remove root controller, since it was basically a hello world. I will
    re-add this later as a sort of index for the API.

--- api/controllers/root.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-# declare blueprint to be registered in application factory
-root = Blueprint("root", __name__)
-
-
-@root.route("/")
-def test_route():
-    return {"message": "Endpoint Exists!"}
