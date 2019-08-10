72d9dee031f50bdb078db6f888eaf88f6f4f1e51
aidan mcbride <aidanmcbride@outlook.com>
2019-08-10 14:48:57 -0600

    Add type checking for get_all_articles return

--- api/controllers/articles.py
+++ api/controllers/articles.py
@@ -1,22 +1,26 @@
 from flask import Blueprint, jsonify, request
 
 from api import db
-from api.models import Article # type: ignore
+from api.models import Article  # type: ignore
+
+# typing imports
+from typing import Tuple
+from flask import Response
 
 articles = Blueprint("articles", __name__, url_prefix="/articles")
 
 
 @articles.route("/", methods=["GET"])
-def get_all_articles():
+def get_all_articles() -> Tuple[Response, int]:
     """
     GET /articles/
     Query database for all articles,
     return all data as json
     """
-    response_data = []
+    response_data = list()
     articles = Article.query.all()
     for article in articles:
-        article_data = {}
+        article_data = dict()
         article_data["id"] = article.id
         article_data["title"] = article.title
         article_data["content"] = article.content
