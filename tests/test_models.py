c29d340adafd6b1d9dc2e3bc9da01b212bcdb815
aidan mcbride <aidanmcbride@outlook.com>
2019-08-12 19:09:27 -0600

    Add test for Article serialization and deserialization

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 from api.models import Article, article_schema
 
@@ -11,7 +11,7 @@ def test_create_article():
     """
 
     # create time here so that actual and expected have same time; if created in model, time will differ.
-    time = datetime.utcnow()
+    time = datetime.now(timezone.utc)
 
     actual = Article(title="Test Title", content="Post body", date_created=time)
     expected = {
@@ -24,3 +24,30 @@ def test_create_article():
     assert actual.title == expected["title"]
     assert actual.slug == expected["slug"]
     assert actual.content == expected["content"]
+
+def test_serialize_article():
+    """
+    GIVEN an Article object created from a dictionary
+    WHEN that object is serialized
+    THEN return a dictionary containing the data from that object, including data that is initialized in the Article constructor
+    """
+    time = datetime.now(timezone.utc)
+    article = Article(title="Test Title", content="Post body", date_created=time)
+
+    actual: dict = article_schema.dump(article).data
+    expected: dict = dict(title="Test Title", slug="test-title", content="Post body", date_created=time.isoformat())
+    for key in expected.keys():
+        assert actual[key] == expected[key]
+
+def test_deserialize_article():
+    time: datetime = datetime.now(timezone.utc)
+    input_time: str = time.isoformat()
+    article_data = dict(title="Test Title", content="Post body", date_created=input_time)
+
+    actual = article_schema.load(article_data).data
+    expected = Article(title="Test Title", content="Post body", date_created=time)
+
+    assert actual.title == expected.title
+    assert actual.slug == expected.slug
+    assert actual.content == expected.content
+    assert actual.date_created == expected.date_created
